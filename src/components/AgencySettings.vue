486b2cc8be5ce683fe806d82e6e17b27728b6c10
Aleksey Denisov <deoz@yandex.ru>
2022-12-07 21:56:04 +0300

    AgencySettings switch edit mode

--- src/components/AgencySettings.vue
+++ src/components/AgencySettings.vue
@@ -5,13 +5,12 @@
     variant="outlined"
     class="mx-auto my-12 pa-6 rounded-lg"
   >
-    <v-form ref="form" v-model="valid" class="pa-8">
+    <v-form ref="form" v-model="valid" class="pa-8" :disabled="!editMode">
       <v-text-field
         v-model="name"
         :rules="nameRules"
         label="Наименование организации"
         variant="outlined"
-        readonly
         required
       />
 
@@ -21,7 +20,6 @@
         :rules="innRules"
         label="ИНН"
         variant="outlined"
-        readonly
         required
       />
 
@@ -29,16 +27,26 @@
         v-model="type"
         :rules="typeRules"
         label="Тип организации"
-        readonly
         mandatory
       >
         <v-radio label="Юридическое лицо" value="ul" />
         <v-radio label="Физическое лицо" value="ip" />
       </v-radio-group>
 
-      <div class="w-100 d-flex justify-end">
+      <div v-if="!editMode" class="w-100 d-flex justify-end">
+        <v-btn color="primary" class="mr-4" @click="enterEditMode"
+          >Редактировать</v-btn
+        >
+      </div>
+      <div v-else class="w-100 d-flex justify-end">
         <v-btn color="success" class="mr-4" @click="validate">Сохранить</v-btn>
-        <v-btn color="error" class="mr-4" @click="reset">Отмена</v-btn>
+        <v-btn
+          color="cancel"
+          variant="outlined"
+          class="mr-4"
+          @click="cancelEditMode"
+          >Отмена</v-btn
+        >
       </div>
     </v-form>
   </v-card>
@@ -49,6 +57,8 @@ export default {
   name: "AgencySettings",
 
   data: () => ({
+    editMode: false,
+
     valid: true,
 
     name: "ООО «Медиаотдел»",
@@ -62,13 +72,17 @@ export default {
   }),
 
   methods: {
+    enterEditMode() {
+      this.editMode = true;
+    },
+    cancelEditMode() {
+      this.editMode = false;
+    },
     async validate() {
       const { valid } = await this.$refs.form.validate();
 
       if (valid) alert("Форма корректна");
-    },
-    reset() {
-      this.$refs.form.reset();
+      this.cancelEditMode();
     },
   },
 };
