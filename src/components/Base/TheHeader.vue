3bc2b912138e975078c39142e216d6b34832c768
Aleksey Denisov <deoz@yandex.ru>
2022-12-13 21:27:47 +0300

    Add Logo image

--- src/components/Base/TheHeader.vue
+++ src/components/Base/TheHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app-bar color="primary" elevation="0" density="compact">
-    <v-app-bar-title>Helen Media Маркировка</v-app-bar-title>
+    <v-app-bar-title>
+      <img src="@/assets/logo.png" />
+    </v-app-bar-title>
   </v-app-bar>
 </template>
 
