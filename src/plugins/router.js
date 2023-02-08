c3ec34b281097b5f09662d64c7e9231f81439325
Aleksey Denisov <deoz@yandex.ru>
2023-02-08 22:19:21 +0300

    auth login redirect

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "@/store";
 import TheLogin from "@/components/Base/TheLogin";
 import The404Page from "@/components/Base/The404Page";
 const ClientsList = () => import("@/components/ClientsList.vue");
@@ -6,14 +7,36 @@ const AgencySettings = () => import("@/components/AgencySettings.vue");
 const CreateReport = () => import("@/components/CreateReport.vue");
 
 const routes = [
-  { path: "/", name: "list", component: ClientsList },
+  {
+    path: "/",
+    name: "list",
+    component: ClientsList,
+    meta: { requiresAuth: true },
+  },
   { path: "/login", name: "login", component: TheLogin },
-  { path: "/report", name: "report", component: CreateReport },
-  { path: "/settings", name: "settings", component: AgencySettings },
+  {
+    path: "/report",
+    name: "report",
+    component: CreateReport,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: AgencySettings,
+    meta: { requiresAuth: true },
+  },
   { path: "/:pathMatch(.*)*", name: "404", component: The404Page },
 ];
 
-export const router = createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach(async (to) => {
+  const canAccess = await store.state.auth.loggedStatus;
+  if (!canAccess && to.meta.requiresAuth) return "/login";
+});
+
+export { router };
